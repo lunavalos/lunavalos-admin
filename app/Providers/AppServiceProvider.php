2a76e77a3da96337262bc2ee182bf98e080832fb
Hugo Luna <hluna88@MacBook-Pro-de-Hugo.local>
2026-03-19 20:20:49 -0600

    Adjust service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +22,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+
+        \Illuminate\Validation\Rules\Password::defaults(function () {
+            return \Illuminate\Validation\Rules\Password::min(6);
+        });
+
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
